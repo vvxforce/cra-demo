c46d9329e0984a526f5c72628875d1f81b263c13
vvxforce <vvxforce@gmail.com>
2019-12-03 09:23:22 +0200

    Updates

---
+++ precache-manifest.b74812e9ab6c5a3778565dc17ee09c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca97adfe102ea1ea53437d966dbacc4",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "034252572c09141ef85a",
+    "url": "/cra-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3433878727a57f68d086",
+    "url": "/cra-demo/static/js/2.4108201c.chunk.js"
+  },
+  {
+    "revision": "034252572c09141ef85a",
+    "url": "/cra-demo/static/js/main.b3603e3d.chunk.js"
+  },
+  {
+    "revision": "51dd5d735150a0d15256",
+    "url": "/cra-demo/static/js/runtime-main.30833128.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cra-demo/static/media/logo.25bf045c.svg"
+  }
+]);
