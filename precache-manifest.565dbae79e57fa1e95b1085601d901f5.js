b80de3b2e71f31bf48145f1157af7fa021442f2b
vvxforce <vvxforce@gmail.com>
2019-12-03 10:12:11 +0200

    Updates

---
+++ precache-manifest.565dbae79e57fa1e95b1085601d901f5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c937506d32f35b7ef3582205a03cd61",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "6ea624aa9b9697035ed0",
+    "url": "/cra-demo/static/js/2.a3e8d21e.chunk.js"
+  },
+  {
+    "revision": "e1da135b4cb63fb2bd17",
+    "url": "/cra-demo/static/js/main.da95eed8.chunk.js"
+  },
+  {
+    "revision": "51dd5d735150a0d15256",
+    "url": "/cra-demo/static/js/runtime-main.30833128.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cra-demo/precache-manifest.b74812e9ab6c5a3778565dc17ee09c9e.js"
+  "/cra-demo/precache-manifest.565dbae79e57fa1e95b1085601d901f5.js"
 );
 
 self.addEventListener('message', (event) => {
